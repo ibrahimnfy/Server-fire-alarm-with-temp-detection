e9dd48ccdb6cc05eb5c7d1e35568b66e661f0410
ibrahimnfy <ibrahimnfy@gmail.com>
2025-08-15 09:26:19 +0300

    last commit i hope

---
+++ protoc-deneme/testclient.cpp
@@ -0,0 +1,3 @@
+#include "testclient.h"
+
+testclient::testclient() {}
---
+++ protoc-deneme/testclient.h
@@ -0,0 +1,10 @@
+#ifndef TESTCLIENT_H
+#define TESTCLIENT_H
+
+class testclient
+{
+public:
+    testclient();
+};
+
+#endif // TESTCLIENT_H
